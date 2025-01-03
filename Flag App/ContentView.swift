47282635a6f6d84bf940880f28efbaa728e5afc3
Pete Nealley <petenealley@yahoo.com>
2025-01-03 17:47:14 -0500

    Initial Commit

---
+++ Flag App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flag App
+//
+//  Created by Pete Nealley on 1/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flag App/Flag_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_AppApp.swift
+//  Flag App
+//
+//  Created by Pete Nealley on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Flag_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
