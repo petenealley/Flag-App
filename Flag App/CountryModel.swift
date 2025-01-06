237110d4668d2e52a7ae6c6c57e3d2e19e393275
Pete Nealley <petenealley@yahoo.com>
2025-01-05 20:10:40 -0500

    Created new branch for converting code to MVMM

---
+++ Flag App/CountryModel.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  Flag App
+//
+//  Created by Pete Nealley on 1/5/25.
+//
+
+import Foundation
+
+struct Country: Identifiable {
+    let id: String
+    let name: String
+    let flag: String
+}
---
+++ Flag App/QuizModel.swift
@@ -0,0 +1,12 @@
+//
+//  QuizModel.swift
+//  Flag App
+//
+//  Created by Pete Nealley on 1/5/25.
+//
+
+import Foundation
+
+struct QuizData {
+    var remainingCountries: [Country]
+}
